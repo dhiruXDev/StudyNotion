8227162ce9f550077910164adb041b411d55a73c
Dhiraj kumar <dhirajk22410@gmail.com>
2025-02-05 01:31:18 +0530

    Update SidebarLink.jsx

--- src/Components/core/Dashboard/SidebarLink.jsx
+++ src/Components/core/Dashboard/SidebarLink.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import * as Icons from "react-icons/vsc"
 import { useDispatch } from 'react-redux';
 import { matchPath, NavLink, useLocation } from 'react-router-dom';
+import { resetCourseState } from '../../../slices/courseSlice';
 export const SidebarLink = ({links, IconName ,setIsOpen}) => {
     const Icon = Icons[IconName] ;          // By using the name og Icon as string i imported Icon 
     const dispatch = useDispatch();
@@ -12,7 +13,8 @@ export const SidebarLink = ({links, IconName ,setIsOpen}) => {
     }
 
   return (
-    <NavLink   to={links.path} className={` ${matchRoute(links.path) ? " bg-yellow-800 border-l-[3px] border-yellow-200 text-yellow-25" : " bg-transparent hover:bg-richblack-700"}  py-3 relative px-2   duration-200 `}>
+    <NavLink   to={links.path} onClick={link.path === "/dashboard/add-course" ? (()=>dispatch(resetCourseState())):null}
+        className={` ${matchRoute(links.path) ? " bg-yellow-800 border-l-[3px] border-yellow-200 text-yellow-25" : " bg-transparent hover:bg-richblack-700"}  py-3 relative px-2   duration-200 `}>
             {/* <span> border ke liye</span> */}
             <div onClick={()=>setIsOpen(false)} className=' flex gap-x-4 items-center font-medium pl-4'>
                     <Icon className='text-xl' />
